ee2122e238489195585e43f5c9a6fb40b425b432
vijju <vijju@vijjus-MacBook-Air.local>
2024-08-12 16:44:33 +0530

    update pagination state

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,67 +1,14 @@
-import React from 'react';
+import React, {  useState }  from 'react';
 import './Pagination.css';
 
-const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
-  // const pageNumbers = [];
-  // for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
-  //   pageNumbers.push(i);
-  // }
 
-  //const [itemsPerPage] = useState(10);
+const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
+  
   const [currentPage, setCurrentPage] = useState(1);
 
   const indexOfLastTrade = currentPage * itemsPerPage;
   const indexOfFirstTrade = indexOfLastTrade - itemsPerPage;
   const currentTrades = tradesWithCumulativePL.slice(indexOfFirstTrade, indexOfLastTrade);
-
-  // const totalItems = Math.ceil(tradesWithCumulativePL.length / itemsPerPage);
-
-  // const renderPageNumbers = () => {
-  //   if (pageNumbers.length <= 5) {
-  //     return pageNumbers.map(number => (
-  //       <li key={number}>
-  //         <button
-  //           onClick={() => paginate(number)}
-  //           className={currentPage === number ? 'active' : ''}
-  //         >
-  //           {number}
-  //         </button>
-  //       </li>
-  //     ));
-  //   } else {
-  //     const dots = '...';
-  //     const firstPage = pageNumbers[0];
-  //     const lastPage = pageNumbers[pageNumbers.length - 1];
-  //     const currentPageGroupStart = Math.max(currentPage - 1, 2);
-  //     const currentPageGroupEnd = Math.min(currentPage + 1, pageNumbers.length - 1);
-
-  //     const pages = [
-  //       firstPage,
-  //       currentPageGroupStart > 2 && dots,
-  //       ...pageNumbers.slice(currentPageGroupStart - 1, currentPageGroupEnd),
-  //       currentPageGroupEnd < pageNumbers.length - 1 && dots,
-  //       lastPage,
-  //     ];
-
-  //     return pages.map((number, index) =>
-  //       number === dots ? (
-  //         <li key={`dots-${index}`}>
-  //           <button disabled>{number}</button>
-  //         </li>
-  //       ) : (
-  //         <li key={number}>
-  //           <button
-  //             onClick={() => paginate(number)}
-  //             className={currentPage === number ? 'active' : ''}
-  //           >
-  //             {number}
-  //           </button>
-  //         </li>
-  //       )
-  //     );
-  //   }
-  // };
-
 
 const prevPage = () => {
   setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
